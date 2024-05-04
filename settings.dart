6325e5cc70e89268fb09cc20f33531ee29f5266c
Lujainms <157852818+Lujainms@users.noreply.github.com>
2024-05-04 21:05:53 +0300

    Update settings.dart

--- settings.dart
+++ settings.dart
@@ -30,65 +30,78 @@ class _SettingsScreenState extends State<SettingsScreen> {
           'اعدادات اللعبة',
           style: TextStyle(color: Colors.brown), // Brown color for app bar title
         ),
+        centerTitle: true, // Center align the title
+        backgroundColor: const Color(0xFF966C50), // Background color of the AppBar
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.brown), // Brown color for back icon
+          icon: const Icon(Icons.arrow_back, color: Colors.brown), // Brown color for back icon
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
-      body: Center( // Center widget to align everything in the middle
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Centering vertically
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end, // Aligns text to the right
-                children: [
-                  Text(
-                    'السطوع',
-                    style: TextStyle(fontSize: 20, color: Colors.brown), // Brown color
-                  ),
-                ],
-              ),
-              Slider(
-                value: brightnessValue,
-                onChanged: (value) {
-                  setState(() {
-                    brightnessValue = value;
-                  });
-                },
-                activeColor: Colors.brown, // Set the active color of the Slider to brown
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end, // Aligns text to the right
-                children: [
-                  Text(
-                    'الصوت',
-                    style: TextStyle(fontSize: 20, color: Colors.brown), // Brown color
-                  ),
-                ],
-              ),
-              SizedBox(height: 10), // Added space between text and Switch
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end, // Aligns Switch to the right
-                children: [
-                  Switch(
-                    value: soundEnabled,
-                    onChanged: (value) {
-                      setState(() {
-                        soundEnabled = value;
-                      });
-                    },
-                    activeTrackColor: Colors.brown, // Brown color for Switch active track
-                    activeColor: Colors.white, // Brown color for Switch thumb when active
-                  ),
-                ],
-              ),
-            ],
+      body: Container( // Added Container as a parent widget
+        decoration: const BoxDecoration( // Decoration for the Container to set the background image
+          image: DecorationImage(
+            image: AssetImage('images/مفازة.png'), // Replace 'background_image.jpg' with your asset path
+            fit: BoxFit.cover, // Cover the entire container
+          ),
+        ),
+        child: Center( // Center widget to align everything in the middle
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Centering vertically
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end, // Aligns text to the right
+                  children: [
+                    Text(
+                      'السطوع',
+                      style: TextStyle(fontSize: 20, color: Colors.brown), // Brown color
+                    ),
+                  ],
+                ),
+                Slider(
+                  value: brightnessValue,
+                  onChanged: (value) {
+                    setState(() {
+                      brightnessValue = value;
+                    });
+                  },
+                  activeColor: Colors.brown, // Set the active color of the Slider to brown
+                ),
+                const SizedBox(height: 20),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end, // Aligns text to the right
+                  children: [
+                    Text(
+                      'الصوت',
+                      style: TextStyle(fontSize: 20, color: Colors.brown), // Brown color
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10), // Added space between text and Switch
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center, // Aligns Switch to the center
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Switch(
+                        value: soundEnabled,
+                        onChanged: (value) {
+                          setState(() {
+                            soundEnabled = value;
+                          });
+                        },
+                        activeTrackColor: Colors.brown, // Brown color for Switch active track
+                        activeColor: Colors.white, // Brown color for Switch thumb when active
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
